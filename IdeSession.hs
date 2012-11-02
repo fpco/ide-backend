a889a0c9feb437517eb5be41d0e164398f7a2c86
Duncan Coutts <duncan@well-typed.com>
2012-11-02 10:46:14 +0000

    A few updates to the IdeSession API
    Various extra bits of documetation.
    Add a function to wait for the future completion.
    Add query functions to read the source and data files.

--- IdeSession.hs
+++ IdeSession.hs
@@ -2,7 +2,7 @@ module IdeSession (
 
   -- | This module provides an interface to the IDE backend.
   --
-  -- It centers around the idea of a single threaded IDE session, and
+  -- It centres around the idea of a single threaded IDE session, and
   -- operations for updating the session or running queries given the current
   -- state of the session.
 
@@ -13,7 +13,7 @@ module IdeSession (
   -- with the state of files in the file system which are of course mutable
   -- variables.
   --
-  -- So the general pattern of interation is sequential and single-threaded.
+  -- So the general pattern of interaction is sequential and single-threaded.
   -- The state transitions are fairly simple:
   --
   -- * update phase: we have a batch of updates, e.g. changes in module contents.
@@ -38,9 +38,9 @@ module IdeSession (
 
   -- * Sessions
   IdeSession,
-  
+
   -- ** Initialisation and shutdown
-  -- | Sessions are staterful and must be initialised and shut down so that
+  -- | Sessions are stateful and must be initialised and shut down so that
   -- resources can be released.
   initSession,
   shutdownSession,
@@ -72,8 +72,16 @@ module IdeSession (
   -- ** Data files
   updateDataFile,
   DataFileChange(..),
-  
+
   -- * Queries
+  Query,
+
+  -- ** Files
+  -- | Simply getting the current state of the persistent files fits the
+  -- queries pattern.
+  getSourceModule,
+  getDataFile,
+
   -- ** Source errors
   getSourceErrors,
   SourceError,
@@ -83,15 +91,53 @@ module IdeSession (
   SymbolDefinitionMap,
 
   -- * Additional notes
-  -- ** Responsibilty for managing and mutating files in the sources dir.
+  -- ** Responsibility for managing and mutating files in the sources dir.
   -- | In general, updating and changing source files in the sources dir has to
   -- be coordinated with the IdeSession, since we're in a concurrent mutable
   -- setting.
   --
-  -- The compromise is that the caller can manage the files prior to initialising
-  -- a session, but while the session is active, all file changes must be managed
+  -- The model here is that the IdeSession alone manages the files in the
+  -- sources directory. All file changes and file reading must be managed
   -- via the session, and sequenced relative to other session state changes.
   --
+  -- The session will manage the files carefully, including in the case of
+  -- exceptions and things going awry. Thus the caller does not need to
+  -- duplicate the file state: it can rely on putting files in, applying
+  -- updates to the files via the session, and extracting the files again
+  -- at any time.
+
+  -- ** Morally pure queries
+  -- | Morally, a compiler is a pure function from the current value of the
+  -- various source files (and other bits of the environment) to object code
+  -- and\/or other information about the modules (errors, types etc).
+  --
+  -- The intention is to reflect this purity property in this interface. The
+  -- value of an 'IdeSession' represents the state of the files\/modules and
+  -- the result of the pure compilation function. It should always be the case
+  -- that we can throw away the session and recover it just from the persistent
+  -- state in the files.
+  --
+  -- One example where this notion makes a difference is with warnings.
+  -- Traditionally, compilers just return the warnings for the modules they
+  -- compiled, skipping warnings for the modules they didn't need to recompile.
+  -- But this doesn't match the pure function idea, because now the compilation
+  -- result now depends on which steps we took to get there, rather than just
+  -- on the current value of the files. So one of the things this wrapper can
+  -- do is to restore the purity in these corner cases, (which otherwise the
+  -- client of this API would probably have to do).
+
+  -- ** Persistent and transitory state
+  -- | The persistent state is obviously the files: source files and data
+  -- files. Internally there is a great deal of transitory and cached state,
+  -- either in memory or on disk (such as .hi files on disk or the equivalent
+  -- in memory).
+  --
+  -- It should always be possible to drop all the transitory state and recover,
+  -- just at the cost of some extra work.
+  --
+  -- This property is a useful correctness property for internal testing: the
+  -- results of all the queries should be the same before and after blowing
+  -- away all the transitory state and recovering.
 
 ) where
 
@@ -105,11 +151,12 @@ import qualified Data.ByteString.Lazy as BS
 -- of updates, ultimately leading to a new 'IdeSession'.
 --
 -- Note that these state tokens are not persistent, once we perform an update
--- and get a new 'IdeSession', the old one is no longer valid.
+-- and get a new 'IdeSession', the old one is no longer valid. (And if you do
+-- accidentally use an old invalid one, then it's a dynamically checked error.)
 --
 data IdeSession
 
--- | Configuration paramaters for a session. These remain the same throughout
+-- | Configuration parameters for a session. These remain the same throughout
 -- the whole session's lifetime.
 --
 data SessionConfig = SessionConfig {
@@ -124,7 +171,7 @@ data SessionConfig = SessionConfig {
        -- running program. The running program will have this as its CWD.
        configDataDir :: FilePath,
 
-       -- | The directroy to use for purely temporary files.
+       -- | The directory to use for purely temporary files.
        configTempDir :: FilePath
      }
 
@@ -160,7 +207,15 @@ updateSession = undefined
 
 -- | A future, a handle on an action that will produce some result.
 --
+-- Currently this is just a simple future, but there is the option to extend
+-- it with actual progress info, and\/or with cancellation.
+--
 data Progress a
+
+-- | Block until the operation completes.
+--
+progressWaitCompletion :: Progress a -> IO a
+progressWaitCompletion = undefined
 
 -- | A session update that changes a source module. Modules can be added,
 -- updated or deleted.
@@ -188,12 +243,24 @@ data DataFileChange = DataFilePut    FilePath ByteString
 --
 type Query a = IdeSession -> IO a
 
+-- | Read the current value of one of the source modules.
+--
+getSourceModule :: ModuleName -> Query ByteString
+getSourceModule = undefined
+
+-- | Read the current value of one of the data files.
+--
+getDataFile :: FilePath -> Query ByteString
+getDataFile = undefined
+
 -- | Get any compilation errors or warnings in the current state of the
 -- session, meaning errors that GHC reports for the current state of all the
 -- source modules.
 --
--- Note that it is all errors and warnings in the current state, not just extra
--- errors from the last session change. In particular this applies to warnings.
+-- Note that in the initial implementation this will only return warnings from
+-- the modules that changed in the last update, the intended semantics is that
+-- morally it be a pure function of the current state of the files, and so it
+-- would return all warnings (as if you did clean and rebuild each time).
 --
 getSourceErrors :: Query [SourceError]
 getSourceErrors = undefined
@@ -203,10 +270,15 @@ getSourceErrors = undefined
 -- Most errors are associated with a span of text, but some have only a
 -- location point.
 --
+-- * This is currently a stub, but it will be a full concrete type.
+--
 data SourceError
 
 
 -- | A mapping from symbol uses to symbol definitions
+--
+-- * This is currently a stub, but it will be a full concrete type so that
+-- it can be serialised etc.
 --
 data SymbolDefinitionMap
 
