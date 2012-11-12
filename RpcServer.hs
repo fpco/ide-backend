0675cf2fed57947ae40a8b2e6d6416835a0e9efa
Edsko de Vries <edsko@well-typed.com>
2012-11-12 17:32:22 +0000

    Flesh out the skeleton
    Not yet working though

--- RpcServer.hs
+++ RpcServer.hs
@@ -1,8 +1,24 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall #-}
-module RpcServer where
+module RpcServer 
+  ( -- * Server-side
+    rpcServer
+    -- * Client-side
+  , RpcServer
+  , forkRpcServer
+  , rpc 
+    -- * Testing
+  , main
+  ) where
 
-import System.IO (Handle, hSetBinaryMode, stderr, hPrint)
+import System.IO (Handle, hSetBinaryMode, stdin, stdout, stderr, hPrint)
+import System.Process 
+  ( CreateProcess(std_in, std_out, std_err)
+  , createProcess
+  , proc
+  , StdStream(CreatePipe)
+  , ProcessHandle
+  )
 import Data.Aeson 
   ( FromJSON
   , ToJSON
@@ -15,10 +31,19 @@ import Control.Monad (forever)
 import qualified Control.Exception as Ex (SomeException, throwIO, catch)
 import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, readMVar)
+import Control.Concurrent.MVar 
+  ( MVar
+  , newMVar
+  , newEmptyMVar
+  , putMVar
+  , readMVar
+  , modifyMVar 
+  )
 import qualified Data.ByteString.Lazy as BSL (ByteString, hPut, hGetContents)
-import Data.Attoparsec.ByteString (Parser)
 import Data.Attoparsec.ByteString.Lazy (parse, Result(Done, Fail))
+
+-- For testing
+import System.Environment (getArgs, getProgName)
 
 --------------------------------------------------------------------------------
 -- Server-side API                                                            --
@@ -47,18 +72,48 @@ rpcServer inp outp handler = do
 -- Client-side API                                                            --
 --------------------------------------------------------------------------------
 
-data RpcServer req resp
+-- | Abstract data type representing RPC servers
+data RpcServer req resp = RpcServer {
+    -- | The server's 'stdin'
+    rpcIn :: Handle               
+    -- | The server's 'stdout' 
+    --
+    -- This represents the parser state, hence we represent it as an MVar
+    -- around a lazy bytestring (the MVar doubles as the lock for the server,
+    -- sequentializing concurrent requests)
+  , rpcOut :: MVar BSL.ByteString  
+    -- | The server's 'stderr'
+  , rpcErr :: Handle              
+    -- | Handle on the server process itself
+  , rpcProc :: ProcessHandle
+  }
 
 -- | Fork an RPC server as a separate process
-forkRpcServer :: FilePath -> IO (RpcServer req resp)
-forkRpcServer = undefined
+forkRpcServer :: FilePath  -- ^ Filename of the executable
+              -> [String]  -- ^ Arguments
+              -> IO (RpcServer req resp)
+forkRpcServer path args = do
+  (Just hin, Just hout, Just herr, ph) <- createProcess $ (proc path args) { 
+      std_in  = CreatePipe
+    , std_out = CreatePipe
+    , std_err = CreatePipe
+    }
+  contents     <- BSL.hGetContents hout
+  contentsMVar <- newMVar contents
+  return (RpcServer hin contentsMVar herr ph)
 
 -- | Do an RPC call
-rpc :: (FromJSON req, ToJSON resp)
+--
+-- Throws an exception if the server returns a mallformed result
+rpc :: (ToJSON req, FromJSON resp)
     => RpcServer req resp  -- ^ RPC server
     -> req                 -- ^ Request
     -> IO resp             -- ^ Response
-rpc = undefined
+rpc server req = modifyMVar (rpcOut server) $ \out -> do
+  BSL.hPut (rpcIn server) (encode req)
+  case parseJSON out of
+    Right (out', resp) -> return (out', resp)
+    Left err           -> Ex.throwIO (userError err)
 
 --------------------------------------------------------------------------------
 -- Internal                                                                   --
@@ -73,21 +128,23 @@ readRequests h ch = do
   where
     go :: BSL.ByteString -> IO ()
     go contents = 
-      case parse parser contents of
-        Done contents' req -> do
+      case parseJSON contents of
+        Right (contents', req) -> do
           writeChan ch req
           go contents'
-        Fail _ _ err ->
+        Left err ->
           -- Exception will be caught by 'rpcServer'
           Ex.throwIO (userError err)
     
-    parser :: Parser req
-    parser = do
-      value <- json'
+parseJSON :: FromJSON a => BSL.ByteString -> Either String (BSL.ByteString, a) 
+parseJSON bs =
+  case parse json' bs of
+    Fail _ _ err -> Left err
+    Done bs' value -> 
       case fromJSON value of
-        Success req -> return req
-        Error err   -> fail err
-          
+        Success req -> Right (bs', req)
+        Error err   -> Left err
+    
 -- | Encode messages from a channel and forward them on a handle
 writeResponses :: ToJSON resp => Chan resp -> Handle -> IO ()
 writeResponses ch h = forever $ readChan ch >>= BSL.hPut h .  encode 
@@ -103,5 +160,16 @@ channelHandler inp outp handler = forever $ do
 -- For testing                                                                --
 --------------------------------------------------------------------------------
 
+echoServer :: String -> IO String
+echoServer = return
+
 main :: IO ()
-main = putStrLn "Hi"
+main = do
+  args <- getArgs
+  case args of
+    ["--server"] -> rpcServer stdin stdout echoServer   
+    _ -> do
+      prog <- getProgName 
+      server <- forkRpcServer prog []
+      resp   <- rpc server "Hello server"
+      putStrLn resp
