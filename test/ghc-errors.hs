6a649afad4d074d3f2e75fa14f9533ed912e9ddd
Mikolaj Konarski <mikolaj@well-typed.com>
2012-12-06 14:18:48 +0100

    Merge ghc-features.hs and ghc-errors.hs

--- test/ghc-errors.hs
+++ test/ghc-errors.hs
@@ -12,7 +12,7 @@ import Data.Maybe (isJust)
 
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
-import Test.HUnit (Assertion, assertBool)
+import Test.HUnit (Assertion, assertBool, assertEqual)
 
 import IdeSession
 import GhcServer
@@ -50,7 +50,7 @@ testSingle opts originalSourcesDir check =
         -- in case the default value of CodeGeneration changes.
         originalUpdate = updateModule (ChangeCodeGeneration False)
                          <> (mconcat $ map upd originalModules)
-    s1 <- updateSessionD s0 originalUpdate (progressWaitConsume displayCounter)
+    s1 <- updateSessionD s0 originalUpdate
     -- Perform some API calls and check the results.
     s2 <- check s1
     -- Clean up.
@@ -70,7 +70,7 @@ featureTests =
         -- Overwrite one of the copied files.
         (m, _) <- getModules s1
         let update = loadModule m "a = unknownX"
-        s2 <- updateSessionD s1 update (progressWaitConsume displayCounter)
+        s2 <- updateSessionD s1 update
         msgs <- getSourceErrors s2
         assertBool "Type error lost" $ length msgs >= 1
         return s2
@@ -81,7 +81,7 @@ featureTests =
         let upd m =
               updateModule (ModulePut m (pack "module Wrong where\na = 1"))
             update = mconcat $ map upd lm
-        s2 <- updateSessionD s1 update (progressWaitConsume displayCounter)
+        s2 <- updateSessionD s1 update
         msgs <- getSourceErrors s2
         assertBool "Wrong module name not caught" $ length msgs >= 1
         return s2
@@ -93,16 +93,16 @@ featureTests =
         let update1 =
               updateModule (ChangeCodeGeneration False)
               <> loadModule m1 "a = unknownX"
-        s2 <- updateSessionD s1 update1 (progressWaitConsume displayCounter)
-        s3 <- updateSessionD s2 mempty (progressWaitConsume displayCounter)
+        s2 <- updateSessionD s1 update1
+        s3 <- updateSessionD s2 mempty
         -- Overwrite all files, many times, with correct modules.
         let upd m = loadModule m "x = 1"
                     <> updateModule (ChangeCodeGeneration True)
                     <> loadModule m "y = 2"
             update2 = mconcat $ map upd lm
-        s4 <- updateSessionD s3 update2 (progressWaitConsume displayCounter)
+        s4 <- updateSessionD s3 update2
         -- Overwrite again with the error.
-        s5 <- updateSessionD s4 update1 (progressWaitConsume displayCounter)
+        s5 <- updateSessionD s4 update1
         msgs5 <- getSourceErrors s5
         assertBool "Type error lost" $ length msgs5 >= 1
         assertBool ("Too many type errors: "
@@ -120,7 +120,7 @@ featureTests =
     , ("Run the sample code; don't fail without an explanation"
       , \s1 -> do
         let update = updateModule (ChangeCodeGeneration True)
-        s2 <- updateSessionD s1 update (progressWaitConsume displayCounter)
+        s2 <- updateSessionD s1 update
         (msgs, resOrEx) <- runStmt s2 "Main" "main"
         assertBool "No errors detected, but the run failed" $
           case resOrEx of
@@ -139,7 +139,7 @@ featureTests =
                               (pack "module Main where\nmain = return ()"))
               <> mconcat (map upd lm)
               <> updateModule (ChangeCodeGeneration True)
-        s2 <- updateSessionD s1 update (progressWaitConsume displayCounter)
+        s2 <- updateSessionD s1 update
         (msgs, resOrEx) <- runStmt s2 "Main" "main"
         assertBool ("Manually corrected code not run successfully: "
                     ++ List.intercalate "\n" (map formatSourceError msgs)) $
@@ -184,6 +184,14 @@ featureTests =
   shutdownSession s12
 -}
 
+-- | Test that the list of successfully compiled modules is reported correctly
+testListCompiledModules :: Assertion
+testListCompiledModules = testSingle defOpts "." $ \s1 -> do
+    let m = ModuleName "A"
+    s2 <- updateSessionD s1 (loadModule m "")
+    assertEqual "" [m] =<< getLoadedModules s2
+    return s2
+
 defOpts :: [String]
 defOpts = [ "-no-user-package-conf" ]
 
@@ -225,8 +233,11 @@ tests =
       caseFeature originalSourcesDir opts (featureName, check) =
         testCase featureName
         $ testSingle opts originalSourcesDir check
-  in [ testGroup "Full integration tests"
+  in [ testGroup "Full integration tests on multiple project"
        $ map groupProject projects
+     , testGroup "Synthetic integration tests"
+         [ testCase "Maintain list of compiled modules" testListCompiledModules
+         ]
      ]
 
 main :: IO ()
@@ -242,11 +253,9 @@ main = do
 displayCounter :: PCounter -> IO ()
 displayCounter n = debug dVerbosity $ "PCounter: " ++ (show n) ++ ". "
 
-updateSessionD :: IdeSession -> IdeSessionUpdate
-               -> (Progress PCounter IdeSession -> IO IdeSession)
-               -> IO IdeSession
-updateSessionD s0 update handler = do
-  s1 <- updateSession s0 update handler
+updateSessionD :: IdeSession -> IdeSessionUpdate -> IO IdeSession
+updateSessionD s0 update = do
+  s1 <- updateSession s0 update (progressWaitConsume displayCounter)
   msgs <- getSourceErrors s1
   debug dVerbosity $ "getSourceErrors after update: "
                      ++ List.intercalate "\n" (map formatSourceError msgs)
