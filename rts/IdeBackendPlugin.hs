a204ac419b5c35e9192489b657ebf551b495639c
Edsko de Vries <edsko@well-typed.com>
2013-02-11 15:51:22 +0000

    Second example from the docs, modified for ghc 7.6

--- rts/IdeBackendPlugin.hs
+++ rts/IdeBackendPlugin.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module IdeBackendPlugin (plugin) where
 
 import GhcPlugins
@@ -10,5 +11,19 @@ plugin = defaultPlugin {
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todo = do
   reinitializeGlobals
-  liftIO $ appendFile "/tmp/ghc.log" "Hello!"
-  return todo
+  return (CoreDoPluginPass "IdeBackend" pass : todo)
+
+pass :: ModGuts -> CoreM ModGuts
+pass guts = do
+    dynFlags <- getDynFlags
+    bindsOnlyPass (mapM $ printBind dynFlags) guts
+  where 
+    printBind :: DynFlags -> CoreBind -> CoreM CoreBind
+    printBind _dynFlags bndr@(NonRec b _) = do
+#if __GLASGOW_HASKELL__ >= 706
+      liftIO $ appendFile "/tmp/ghc.log" $ showSDoc _dynFlags (ppr b)
+#else
+      liftIO $ appendFile "/tmp/ghc.log" $ showSDoc (ppr b)
+#endif
+      return bndr 
+    printBind _dynFlags bndr = return bndr
