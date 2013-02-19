ecf2a456f8761ab59859668b3948a71af963f3fc
Edsko de Vries <edsko@well-typed.com>
2013-02-19 14:01:53 +0000

    Start extracting info from types

--- GhcHsWalk.hs
+++ GhcHsWalk.hs
@@ -8,7 +8,8 @@ module GhcHsWalk
   , haddockLink
   ) where
 
-import Prelude hiding (span, id)
+import Prelude hiding (span, id, mod)
+import Control.Monad (forM_)
 import Control.Monad.Writer (MonadWriter, WriterT, execWriterT, tell)
 import Control.Monad.Trans.Class (MonadTrans, lift)
 import Data.IORef
@@ -105,12 +106,11 @@ instance Show IdMap where
         ++ (case idType of Nothing -> " (type unknown)" ; Just tp -> tp)
         ++ " (" ++ takeFileName (ppSpan idDefSpan) ++ ")"
 
+-- Right-biased union (information provided by the type checker replaces
+-- information provided by the renamer)
 instance Monoid IdMap where
   mempty = IdMap Map.empty
-  (IdMap a) `mappend` (IdMap b) = IdMap (Map.unionWith combineIdInfo a b)
-
-combineIdInfo :: IdInfo -> IdInfo -> IdInfo
-combineIdInfo _ _ = error "This should not (yet) happen"
+  (IdMap a) `mappend` (IdMap b) = IdMap (Map.union b a)
 
 fromGhcNameSpace :: Name.NameSpace -> IdNameSpace
 fromGhcNameSpace ns =
@@ -147,6 +147,22 @@ haddockLink (IdMap m) sp =
   Extract an IdMap from information returned by the ghc type checker
 ------------------------------------------------------------------------------}
 
+extractIdsPlugin :: IORef [IdMap] -> HscPlugin
+extractIdsPlugin symbolRef = HscPlugin $ \env -> do
+  identMap <- execExtractIdsT $ do extractIds (tcg_rn_decls env)
+                                   extractIds (tcg_binds env)
+
+  liftIO $ appendFile "/tmp/ghc.log" (show identMap)
+  liftIO $ modifyIORef symbolRef (identMap :)
+
+  return env
+
+{------------------------------------------------------------------------------
+  ExtractIdsT is just a wrapper around the writer monad for IdMap
+  (we wrap it to avoid orphan instances). Note that MonadIO is GHC's
+  MonadIO, not the standard one, and hence we need our own instance.
+------------------------------------------------------------------------------}
+
 -- Define type synonym to avoid orphan instances
 newtype ExtractIdsT m a = ExtractIdsT { runExtractIdsT :: WriterT IdMap m a }
   deriving (Functor, Monad, MonadWriter IdMap, MonadTrans)
@@ -175,38 +191,36 @@ record span isBinder id = do
     Right unhelpful -> fail $ "Id without sourcespan: " ++ unhelpful
   tell . IdMap . Map.singleton sourceSpan $ constructIdInfo dynFlags isBinder id
 
-extractIdsPlugin :: IORef [IdMap] -> HscPlugin
-extractIdsPlugin symbolRef = HscPlugin $ \env -> do
-  identMap <- execExtractIdsT $ extractIds (tcg_binds env)
+-- We should ignore unrecognized expressions rather than throw an error
+-- However, for writing this code in the first place it's useful to know
+-- which constructor we fail to support
+unsupported :: Monad m => String -> ExtractIdsT m ()
+unsupported c = fail $ "extractIds: unsupported " ++ c
 
-  debugPP "tcg_rn_decls" (tcg_rn_decls env)
-
-  liftIO $ appendFile "/tmp/ghc.log" (show identMap)
-  liftIO $ modifyIORef symbolRef (identMap :)
-
-  return env
-
-{-------------------------------------------------------------------------------
+{------------------------------------------------------------------------------
   ConstructIdInfo
--------------------------------------------------------------------------------}
+------------------------------------------------------------------------------}
 
-class ConstructIdInfo id where
+class OutputableBndr id => ConstructIdInfo id where
   constructIdInfo :: DynFlags -> IsBinder -> id -> IdInfo
 
 instance ConstructIdInfo Id where
-  constructIdInfo dynFlags idIsBinder id = IdInfo{..}
+  constructIdInfo dynFlags idIsBinder id =
+    (constructIdInfo dynFlags idIsBinder (Var.varName id)) {
+        idType = Just . showSDoc dynFlags . ppr . Var.varType $ id
+      }
+
+instance ConstructIdInfo Name where
+  constructIdInfo _ idIsBinder name = IdInfo{..}
     where
-      nameStruct = Var.varName id
-      occStruct = Name.nameOccName nameStruct
-      moduleStruct = Name.nameModule_maybe nameStruct
-      idName    = Name.occNameString occStruct
-      idModule  =
-        fmap (Module.moduleNameString . Module.moduleName) moduleStruct
-      idPackage =
-        fmap (Module.packageIdString . Module.modulePackageId) moduleStruct
-      idSpace   = fromGhcNameSpace $ Name.occNameSpace occStruct
-      idType    = Just $ showSDoc dynFlags (ppr $ Var.varType id)
-      idDefSpan = extractSourceSpan (Name.nameSrcSpan nameStruct)
+      occ       = Name.nameOccName name
+      mod       = Name.nameModule_maybe name
+      idName    = Name.occNameString occ
+      idModule  = fmap (Module.moduleNameString . Module.moduleName) mod
+      idPackage = fmap (Module.packageIdString . Module.modulePackageId) mod
+      idSpace   = fromGhcNameSpace $ Name.occNameSpace occ
+      idDefSpan = extractSourceSpan (Name.nameSrcSpan name)
+      idType    = Nothing -- After renamer but before typechecker
 
 {------------------------------------------------------------------------------
   ExtractIds
@@ -215,24 +229,80 @@ instance ConstructIdInfo Id where
 class ExtractIds a where
   extractIds :: (Functor m, MonadIO m, HasDynFlags m) => a -> ExtractIdsT m ()
 
+instance ExtractIds a => ExtractIds [a] where
+  extractIds = mapM_ extractIds
+
+instance ExtractIds a => ExtractIds (Maybe a) where
+  extractIds Nothing  = return ()
+  extractIds (Just x) = extractIds x
+
+instance ConstructIdInfo id => ExtractIds (HsGroup id) where
+  extractIds group = do
+    -- TODO: HsGroup has lots of other fields
+    extractIds (hs_valds group)
+
+instance ConstructIdInfo id => ExtractIds (HsValBinds id) where
+  extractIds (ValBindsIn {}) =
+    fail "extractIds: Unexpected ValBindsIn"
+  extractIds (ValBindsOut binds sigs) = do
+    extractIds (map snd binds)
+    extractIds sigs
+
+instance ConstructIdInfo id => ExtractIds (LSig id) where
+  extractIds (L _span (TypeSig names tp)) = do
+    forM_ names $ \name -> record (getLoc name) NonBinding (unLoc name)
+    extractIds tp
+  extractIds (L _span (GenericSig _ _)) = unsupported "GenericSig"
+  extractIds (L _span (IdSig _))        = unsupported "IdSig"
+  extractIds (L _span (FixSig _))       = unsupported "FixSig"
+  extractIds (L _span (InlineSig _ _))  = unsupported "InlineSig"
+  extractIds (L _span (SpecSig _ _ _))  = unsupported "SpecSig"
+  extractIds (L _span (SpecInstSig _))  = unsupported "SpecInstSig"
+
+instance ConstructIdInfo id => ExtractIds (LHsType id) where
+  extractIds (L _span (HsFunTy arg res)) =
+    extractIds [arg, res]
+  extractIds (L span (HsTyVar name)) =
+    record span NonBinding name
+
+  extractIds (L _span (HsForAllTy _ _ _ _))    = unsupported "HsForAllTy _ _ _"
+  extractIds (L _span (HsAppTy _ _))           = unsupported "HsAppTy _"
+  extractIds (L _span (HsListTy _))            = unsupported "HsListTy"
+  extractIds (L _span (HsPArrTy _))            = unsupported "HsPArrTy"
+  extractIds (L _span (HsTupleTy _ _))         = unsupported "HsTupleTy _"
+  extractIds (L _span (HsOpTy _ _ _))          = unsupported "HsOpTy _ _"
+  extractIds (L _span (HsParTy _))             = unsupported "HsParTy"
+  extractIds (L _span (HsIParamTy _ _))        = unsupported "HsIParamTy _"
+  extractIds (L _span (HsEqTy _ _))            = unsupported "HsEqTy _"
+  extractIds (L _span (HsKindSig _ _))         = unsupported "HsKindSig _"
+  extractIds (L _span (HsQuasiQuoteTy _))      = unsupported "HsQuasiQuoteTy"
+  extractIds (L _span (HsSpliceTy _ _ _))      = unsupported "HsSpliceTy _ _"
+  extractIds (L _span (HsDocTy _ _))           = unsupported "HsDocTy _"
+  extractIds (L _span (HsBangTy _ _))          = unsupported "HsBangTy _"
+  extractIds (L _span (HsRecTy _))             = unsupported "HsRecTy"
+  extractIds (L _span (HsCoreTy _))            = unsupported "HsCoreTy"
+  extractIds (L _span (HsExplicitListTy _ _))  = unsupported "HsExplicitListTy _"
+  extractIds (L _span (HsExplicitTupleTy _ _)) = unsupported "HsExplicitTupleTy _"
+  extractIds (L _span (HsTyLit _))             = unsupported "HsTyLit"
+  extractIds (L _span (HsWrapTy _ _))          = unsupported "HsWrapTy _"
+
 instance ConstructIdInfo id => ExtractIds (LHsBinds id) where
-  extractIds = mapM_ extractIds . bagToList
+  extractIds = extractIds . bagToList
 
 instance ConstructIdInfo id => ExtractIds (LHsBind id) where
   extractIds (L _span bind@(FunBind {})) = do
     record (getLoc (fun_id bind)) Binding (unLoc (fun_id bind))
-    debugPP "wrapper" (fun_co_fn bind)
     extractIds (fun_matches bind)
   extractIds (L _span _bind@(PatBind {})) =
-    fail "extractIds: unsupported PatBind"
+    unsupported "PatBind"
   extractIds (L _span _bind@(VarBind {})) =
-    fail "extractIds: unsupported VarBind"
+    unsupported "VarBind"
   extractIds (L _span bind@(AbsBinds {})) =
     extractIds (abs_binds bind)
 
 instance ConstructIdInfo id => ExtractIds (MatchGroup id) where
   extractIds (MatchGroup matches _postTcType) = do
-    mapM_ extractIds matches
+    extractIds matches
     -- We ignore the postTcType, as it doesn't have location information
 
 instance ConstructIdInfo id => ExtractIds (LMatch id) where
@@ -242,7 +312,7 @@ instance ConstructIdInfo id => ExtractIds (LMatch id) where
 
 instance ConstructIdInfo id => ExtractIds (GRHSs id) where
   extractIds (GRHSs rhss _binds) = do
-    mapM_ extractIds rhss
+    extractIds rhss
     -- TODO: deal with the where clause (`binds`)
 
 instance ConstructIdInfo id => ExtractIds (LGRHS id) where
@@ -254,22 +324,23 @@ instance ConstructIdInfo id => ExtractIds (HsLocalBinds id) where
   extractIds (HsValBinds (ValBindsIn _ _)) =
     fail "extractIds: Unexpected ValBindsIn (after renamer these should not exist)"
   extractIds (HsValBinds (ValBindsOut binds _sigs)) =
-    mapM_ (extractIds . snd) binds -- "fst" is 'rec flag'
+    extractIds (map snd binds) -- "fst" is 'rec flag'
   extractIds (HsIPBinds _) =
-    fail "extractIds: unsupported HsIPBinds"
+    unsupported "HsIPBinds"
 
 instance ConstructIdInfo id => ExtractIds (LHsExpr id) where
   extractIds (L _ (HsPar expr)) =
     extractIds expr
+  extractIds (L _ (ExprWithTySig expr _type)) = do
+    extractIds expr
+    debugPP "ExprWithTySig" _type
   extractIds (L _ (ExprWithTySigOut expr _type)) = do
     extractIds expr
-    debugPP "ExprWithTySigOut" _type
+    debugPP "ExprWithTySig" _type
   extractIds (L _ (HsOverLit _ )) =
     return ()
   extractIds (L _ (OpApp left op _fix right)) = do
-    extractIds left
-    extractIds op
-    extractIds right
+    extractIds [left, op, right]
   extractIds (L span (HsVar id)) =
     record span NonBinding id
   extractIds (L span (HsWrap _wrapper expr)) =
@@ -278,43 +349,39 @@ instance ConstructIdInfo id => ExtractIds (LHsExpr id) where
     extractIds binds
     extractIds expr
 
-  -- We should ignore unrecognized expressions rather than throw an error
-  -- However, for writing this code in the first place it's useful to know
-  -- which constructor we fail to support
-  extractIds (L _ (HsIPVar _ )) = fail "extractIds: unsupported HsIPVar"
-  extractIds (L _ (HsLit _)) = fail "extractIds: unsupported HsLit"
-  extractIds (L _ (HsLam _)) = fail "extractIds: unsupported HsLam"
-  extractIds (L _ (HsLamCase _ _ )) = fail "extractIds: unsupported HsLamCase"
-  extractIds (L _ (HsApp _ _)) = fail "extractIds: unsupported HsApp"
-  extractIds (L _ (NegApp _ _)) = fail "extractIds: unsupported NegApp"
-  extractIds (L _ (SectionL _ _)) = fail "extractIds: unsupported SectionL"
-  extractIds (L _ (SectionR _ _)) = fail "extractIds: unsupported SectionR"
-  extractIds (L _ (ExplicitTuple _ _)) = fail "extractIds: unsupported ExplicitTuple"
-  extractIds (L _ (HsCase _ _)) = fail "extractIds: unsupported HsCase"
-  extractIds (L _ (HsIf _ _ _ _)) = fail "extractIds: unsupported HsIf"
-  extractIds (L _ (HsMultiIf _ _)) = fail "extractIds: unsupported HsMultiIf"
-  extractIds (L _ (HsDo _ _ _)) = fail "extractIds: unsupported HsDo"
-  extractIds (L _ (ExplicitList _ _)) = fail "extractIds: unsupported ExplicitList"
-  extractIds (L _ (ExplicitPArr _ _)) = fail "extractIds: unsupported ExplicitPArr"
-  extractIds (L _ (RecordCon _ _ _)) = fail "extractIds: unsupported RecordCon"
-  extractIds (L _ (RecordUpd _ _ _ _ _)) = fail "extractIds: unsupported RecordUpd"
-  extractIds (L _ (ExprWithTySig _ _)) = fail "extractIds: unsupported ExprWithTySig"
-  extractIds (L _ (ArithSeq _ _ )) = fail "extractIds: unsupported ArithSeq"
-  extractIds (L _ (PArrSeq _ _)) = fail "extractIds: unsupported PArrSeq"
-  extractIds (L _ (HsSCC _ _)) = fail "extractIds: unsupported HsSCC"
-  extractIds (L _ (HsCoreAnn _ _)) = fail "extractIds: unsupported HsCoreAnn"
-  extractIds (L _ (HsBracket _)) = fail "extractIds: unsupported HsBracket"
-  extractIds (L _ (HsBracketOut _ _)) = fail "extractIds: unsupported HsBracketOut"
-  extractIds (L _ (HsSpliceE _)) = fail "extractIds: unsupported HsSpliceE"
-  extractIds (L _ (HsQuasiQuoteE _ )) = fail "extractIds: unsupported HsQuasiQuoteE"
-  extractIds (L _ (HsProc _ _)) = fail "extractIds: unsupported HsProc"
-  extractIds (L _ (HsArrApp _ _ _ _ _)) = fail "extractIds: unsupported HsArrApp"
-  extractIds (L _ (HsArrForm _ _ _)) = fail "extractIds: unsupported HsArrForm"
-  extractIds (L _ (HsTick _ _)) = fail "extractIds: unsupported HsTick"
-  extractIds (L _ (HsBinTick _ _ _)) = fail "extractIds: unsupported HsBinTick"
-  extractIds (L _ (HsTickPragma _ _)) = fail "extractIds: unsupported HsTickPragma"
-  extractIds (L _ (EWildPat)) = fail "extractIds: unsupported EWildPat"
-  extractIds (L _ (EAsPat _ _)) = fail "extractIds: unsupported EAsPat"
-  extractIds (L _ (EViewPat _ _)) = fail "extractIds: unsupported EViewPat"
-  extractIds (L _ (ELazyPat _)) = fail "extractIds: unsupported ELazyPat"
-  extractIds (L _ (HsType _ )) = fail "extractIds: unsupported HsType"
+  extractIds (L _ (HsIPVar _ ))          = unsupported "HsIPVar"
+  extractIds (L _ (HsLit _))             = unsupported "HsLit"
+  extractIds (L _ (HsLam _))             = unsupported "HsLam"
+  extractIds (L _ (HsLamCase _ _ ))      = unsupported "HsLamCase"
+  extractIds (L _ (HsApp _ _))           = unsupported "HsApp"
+  extractIds (L _ (NegApp _ _))          = unsupported "NegApp"
+  extractIds (L _ (SectionL _ _))        = unsupported "SectionL"
+  extractIds (L _ (SectionR _ _))        = unsupported "SectionR"
+  extractIds (L _ (ExplicitTuple _ _))   = unsupported "ExplicitTuple"
+  extractIds (L _ (HsCase _ _))          = unsupported "HsCase"
+  extractIds (L _ (HsIf _ _ _ _))        = unsupported "HsIf"
+  extractIds (L _ (HsMultiIf _ _))       = unsupported "HsMultiIf"
+  extractIds (L _ (HsDo _ _ _))          = unsupported "HsDo"
+  extractIds (L _ (ExplicitList _ _))    = unsupported "ExplicitList"
+  extractIds (L _ (ExplicitPArr _ _))    = unsupported "ExplicitPArr"
+  extractIds (L _ (RecordCon _ _ _))     = unsupported "RecordCon"
+  extractIds (L _ (RecordUpd _ _ _ _ _)) = unsupported "RecordUpd"
+  extractIds (L _ (ArithSeq _ _ ))       = unsupported "ArithSeq"
+  extractIds (L _ (PArrSeq _ _))         = unsupported "PArrSeq"
+  extractIds (L _ (HsSCC _ _))           = unsupported "HsSCC"
+  extractIds (L _ (HsCoreAnn _ _))       = unsupported "HsCoreAnn"
+  extractIds (L _ (HsBracket _))         = unsupported "HsBracket"
+  extractIds (L _ (HsBracketOut _ _))    = unsupported "HsBracketOut"
+  extractIds (L _ (HsSpliceE _))         = unsupported "HsSpliceE"
+  extractIds (L _ (HsQuasiQuoteE _ ))    = unsupported "HsQuasiQuoteE"
+  extractIds (L _ (HsProc _ _))          = unsupported "HsProc"
+  extractIds (L _ (HsArrApp _ _ _ _ _))  = unsupported "HsArrApp"
+  extractIds (L _ (HsArrForm _ _ _))     = unsupported "HsArrForm"
+  extractIds (L _ (HsTick _ _))          = unsupported "HsTick"
+  extractIds (L _ (HsBinTick _ _ _))     = unsupported "HsBinTick"
+  extractIds (L _ (HsTickPragma _ _))    = unsupported "HsTickPragma"
+  extractIds (L _ (EWildPat))            = unsupported "EWildPat"
+  extractIds (L _ (EAsPat _ _))          = unsupported "EAsPat"
+  extractIds (L _ (EViewPat _ _))        = unsupported "EViewPat"
+  extractIds (L _ (ELazyPat _))          = unsupported "ELazyPat"
+  extractIds (L _ (HsType _ ))           = unsupported "HsType"
