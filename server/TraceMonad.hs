dc1340ea401e234c1a70f193ddcc807cea82fc24
Edsko de Vries <edsko@well-typed.com>
2013-10-21 16:36:06 +0100

    Minor addition to TraceMonad

--- server/TraceMonad.hs
+++ server/TraceMonad.hs
@@ -23,3 +23,15 @@ instance TraceMonad IO where
 
 instance TraceMonad Ghc where
   trace = liftIO . trace
+
+-- | We cannot declare that every monad that implements MonadIO also
+-- implements MonadTrace, so we provide a special function for this case
+-- *mumbles hackhackhackhack..*
+traceBlockIO :: MonadIO m => String -> m a -> m a
+traceBlockIO str act = do
+  liftIO $ trace $ "BKB " ++ str
+  result <- act
+  liftIO $ trace $ "BKA " ++ str
+  return result
+
+
