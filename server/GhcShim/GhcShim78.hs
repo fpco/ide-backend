d678339063bad407e67851a6519988ff95f2260c
Edsko de Vries <edsko@well-typed.com>
2013-12-27 14:14:34 +0000

    Take advantage of the fix to #8607
    Closes 149. Closes 151.

--- server/GhcShim/GhcShim78.hs
+++ server/GhcShim/GhcShim78.hs
@@ -679,19 +679,19 @@ instance FoldId id => Fold (LTyClDecl id) where
                             tcdTyVars
                             tcdRhs
                            _tcdFVs)) = astMark alg (Just span) "SynDecl" $ do
-    -- foldId alg tcdLName DefSite
+    foldId alg tcdLName DefSite
     fold alg tcdTyVars
     fold alg tcdRhs
   fold alg (L span (DataDecl tcdLName
                              tcdTyVars
                              tcdDataDefn
                             _tcdFVs)) = astMark alg (Just span) "DataDecl" $ do
-    -- foldId alg tcdLName DefSite -- FIXME (#8607)
+    foldId alg tcdLName DefSite
     fold alg tcdTyVars
     fold alg tcdDataDefn
   fold alg (L span decl@(ClassDecl {})) = astMark alg (Just span) "ClassDecl" $ do
     fold alg (tcdCtxt decl)
-    -- foldId alg (tcdLName decl) DefSite -- FIXME (#8607)
+    foldId alg (tcdLName decl) DefSite
     fold alg (tcdTyVars decl)
     -- Sadly, we don't get location info for the functional dependencies
     fold alg (tcdSigs decl)
@@ -828,7 +828,7 @@ instance Fold thing => Fold (HsWithBndrs thing) where
 instance FoldId id => Fold (LFamilyDecl id) where
   fold alg (L span (FamilyDecl fdInfo fdLName fdTyVars fdKindSig)) = astMark alg (Just span) "FamilyDecl" $ do
     fold alg fdInfo
-    -- foldId alg fdLName DefSite -- FIXME (#8607)
+    foldId alg fdLName DefSite
     fold alg fdTyVars
     fold alg fdKindSig
 
--- test/ghc-errors.hs
+++ test/ghc-errors.hs
@@ -2014,8 +2014,7 @@ syntheticTests = [
                     ])
         updateSessionD session upd 1
         assertNoErrors session
-        -- Here and elsewhere: we temporarily disabled id info for ADT type names (#8607)
-        _fixme session "#8607" $ assertIdInfo session "A" (2,6,2,7) "T" TcClsName "" "main:A" "A.hs@2:6-2:7" "" "binding occurrence"
+        assertIdInfo' session "A" (2,6,2,7) (2,6,2,7) "T" TcClsName [] "main:A" [(GHC742, "A.hs@2:6-2:7"), (GHC78, "A.hs@2:1-2:13")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (2,10,2,13) "MkT" DataName "T" "main:A" "A.hs@2:10-2:13" "" "binding occurrence"
     )
   , ( "Type information 3: Polymorphism"
@@ -2040,10 +2039,10 @@ syntheticTests = [
                     ])
         updateSessionD session upd 1
         assertNoErrors session
-        _fixme session "#8607" $ assertIdInfo session "A" (2,6,2,12) "TMaybe" TcClsName "" "main:A" "A.hs@2:6-2:12" "" "binding occurrence"
+        assertIdInfo' session "A" (2,6,2,12) (2,6,2,12) "TMaybe" TcClsName [] "main:A" [(GHC742, "A.hs@2:6-2:12"), (GHC78, "A.hs@2:1-2:35")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (2,13,2,14) "a" TvName "" "main:A" "A.hs@2:13-2:14" "" "binding occurrence"
         assertIdInfo session "A" (2,17,2,25) "TNothing" DataName "TMaybe a" "main:A" "A.hs@2:17-2:25" "" "binding occurrence"
-        assertIdInfo session "A" (2,28,2,33) "TJust" DataName "a -> TMaybe a" "main:A" "A.hs@2:28-2:33" "" "binding occurrence"
+        assertIdInfo' session "A" (2,28,2,33) (2,28,2,33) "TJust" DataName (allVersions "a -> TMaybe a") "main:A" [(GHC742, "A.hs@2:28-2:33"), (GHC78, "A.hs@2:28-2:35")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (2,34,2,35) "a" TvName "" "main:A" "A.hs@2:13-2:14" "" "defined locally"
         assertIdInfo session "A" (4,1,4,3) "f1" VarName "t -> t" "main:A" "A.hs@4:1-4:3" "" "binding occurrence"
         assertIdInfo session "A" (4,4,4,5) "x" VarName "t" "main:A" "A.hs@4:4-4:5" "" "binding occurrence"
@@ -2092,7 +2091,7 @@ syntheticTests = [
                     ])
         updateSessionD session upd 2
         assertNoErrors session
-        _fixme session "#8607" $ assertIdInfo session "A" (2,6,2,7) "T" TcClsName "" "main:A" "A.hs@2:6-2:7" "" "binding occurrence"
+        assertIdInfo' session "A" (2,6,2,7) (2,6,2,7) "T" TcClsName [] "main:A" [(GHC742, "A.hs@2:6-2:7"), (GHC78, "A.hs@2:1-2:13")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (2,10,2,13) "MkT" DataName "T" "main:A" "A.hs@2:10-2:13" "" "binding occurrence"
         assertIdInfo session "B" (3,1,3,4) "foo" VarName "T" "main:B" "B.hs@3:1-3:4" "" "binding occurrence"
         assertIdInfo session "B" (3,7,3,10) "MkT" DataName "T" "main:A" "A.hs@2:10-2:13" "" "imported from main:A at B.hs@2:1-2:9"
@@ -2513,9 +2512,8 @@ syntheticTests = [
                     ])
         updateSessionD session upd 1
         assertNoErrors session
-        assertIdInfo session "A" (5,28,5,33) "c_sin" VarName "CDouble -> CDouble" "main:A" "A.hs@5:28-5:33" "" "binding occurrence"
+        assertIdInfo' session "A" (5,28,5,33) (5,28,5,33) "c_sin" VarName (allVersions "CDouble -> CDouble") "main:A" [(GHC742, "A.hs@5:28-5:33"), (GHC78, "A.hs@5:1-5:55")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (5,37,5,44) "CDouble" TcClsName "" "base-4.5.1.0:Foreign.C.Types" "<no location info>" "base-4.5.1.0:Foreign.C.Types" "imported from base-4.5.1.0:Foreign.C at A.hs@4:1-4:17"
-        assertIdInfo session "A" (7,21,7,26) "c_sin" VarName "CDouble -> CDouble" "main:A" "A.hs@5:28-5:33" "" "defined locally"
         assertIdInfo session "A" (10,22,10,29) "andBack" VarName "CDouble -> CDouble" "main:A" "A.hs@9:1-9:8" "" "defined locally"
         assertIdInfo session "A" (10,33,10,40) "CDouble" TcClsName "" "base-4.5.1.0:Foreign.C.Types" "<no location info>" "base-4.5.1.0:Foreign.C.Types" "imported from base-4.5.1.0:Foreign.C at A.hs@4:1-4:17"
     )
@@ -2534,9 +2532,9 @@ syntheticTests = [
         updateSessionD session upd 1
         assertNoErrors session
         -- TODO: These types should not contain explicit coercions (#68)
-        assertIdInfo' session "A" (4,3,4,6) (4,3,4,6) "Num" DataName [(GHC742, "GHC.Prim.~# * ($a) Int -> Int -> Expr ($a)"), (GHC78, "(($a) GHC.Prim.~# Int) -> Int -> Expr ($a)")] "main:A" (allVersions "A.hs@4:3-4:6") "" (allVersions "binding occurrence")
+        assertIdInfo' session "A" (4,3,4,6) (4,3,4,6) "Num" DataName [(GHC742, "GHC.Prim.~# * ($a) Int -> Int -> Expr ($a)"), (GHC78, "(($a) GHC.Prim.~# Int) -> Int -> Expr ($a)")] "main:A" [(GHC742, "A.hs@4:3-4:6"), (GHC78, "A.hs@4:3-4:26")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (4,23,4,26) "Int" TcClsName "" "ghc-prim-0.2.0.0:GHC.Types" "<wired into compiler>" "base-4.5.1.0:Data.Int" "wired in to the compiler"
-        assertIdInfo session "A" (7,3,7,7) "Cond" DataName "Expr Bool -> Expr a -> Expr a -> Expr a" "main:A" "A.hs@7:3-7:7" "" "binding occurrence"
+        assertIdInfo' session "A" (7,3,7,7) (7,3,7,7) "Cond" DataName (allVersions "Expr Bool -> Expr a -> Expr a -> Expr a") "main:A" [(GHC742, "A.hs@7:3-7:7"), (GHC78, "A.hs@7:3-7:60")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (7,18,7,19) "a" TvName "" "main:A" "A.hs@7:18-7:19" "" "binding occurrence"
         assertIdInfo' session "A" (7,54,7,58) (7,54,7,58) "Expr" TcClsName [] "main:A" [(GHC742, "A.hs@3:6-3:10"), (GHC78, "A.hs@3:1-7:60")] "" (allVersions "defined locally")
         assertIdInfo session "A" (7,59,7,60) "a" TvName "" "main:A" "A.hs@7:18-7:19" "" "defined locally"
@@ -2562,22 +2560,20 @@ syntheticTests = [
         assertNoErrors session
         -- TODO: we don't get location info for the fundeps
         -- (this is missing from GHC's AST)
-        _fixme session "#8607" $ assertIdInfo session "A" (3,7,3,8) "C" TcClsName "" "main:A" "A.hs@3:7-3:8" "" "binding occurrence"
-            -- See above: type vars without an explicit forall are reported differently in different versions of ghc
+        assertIdInfo' session "A" (3,7,3,8) (3,7,3,8) "C" TcClsName [] "main:A" [(GHC742, "A.hs@3:7-3:8"), (GHC78, "A.hs@3:1-4:16")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (3,9,3,10) "a" TvName "" "main:A" "A.hs@3:9-3:10" "" "binding occurrence"
-            -- GHC 7.7 reports the entire class definition as the defsite for f. (I would consider that a ghc bug)
-        assertIdInfo session "A" (4,3,4,4) "f" VarName "" "main:A" "A.hs@4:3-4:4" "" "defined locally"
+        assertIdInfo' session "A" (4,3,4,4) (4,3,4,4) "f" VarName [] "main:A" [(GHC742, "A.hs@4:3-4:4"), (GHC78, "A.hs@4:3-4:16")] "" (allVersions "defined locally")
         assertIdInfo session "A" (4,8,4,11) "Int" TcClsName "" "ghc-prim-0.2.0.0:GHC.Types" "<wired into compiler>" "base-4.5.1.0:Data.Int" "wired in to the compiler"
         assertIdInfo session "A" (4,15,4,16) "a" TvName "" "main:A" "A.hs@3:9-3:10" "" "defined locally"
-        _fixme session "#8607" $ assertIdInfo session "A" (5,7,5,8) "D" TcClsName "" "main:A" "A.hs@5:7-5:8" "" "binding occurrence"
+        assertIdInfo' session "A" (5,7,5,8) (5,7,5,8) "D" TcClsName [] "main:A" [(GHC742, "A.hs@5:7-5:8"), (GHC78, "A.hs@5:1-6:14")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (5,9,5,10) "a" TvName "" "main:A" "A.hs@5:9-5:10" "" "binding occurrence"
         assertIdInfo session "A" (5,11,5,12) "b" TvName "" "main:A" "A.hs@5:11-5:12" "" "binding occurrence"
-        assertIdInfo session "A" (6,3,6,4) "g" VarName "" "main:A" "A.hs@6:3-6:4" "" "defined locally"
+        assertIdInfo' session "A" (6,3,6,4) (6,3,6,4) "g" VarName [] "main:A" [(GHC742, "A.hs@6:3-6:4"), (GHC78, "A.hs@6:3-6:14")] "" (allVersions "defined locally")
         assertIdInfo session "A" (6,8,6,9) "a" TvName "" "main:A" "A.hs@5:9-5:10" "" "defined locally"
         assertIdInfo session "A" (6,13,6,14) "b" TvName "" "main:A" "A.hs@5:11-5:12" "" "defined locally"
-        _fixme session "#8607" $ assertIdInfo session "A" (7,6,7,9) "Foo" TcClsName "" "main:A" "A.hs@7:6-7:9" "" "binding occurrence"
+        assertIdInfo' session "A" (7,6,7,9) (7,6,7,9) "Foo" TcClsName [] "main:A" [(GHC742, "A.hs@7:6-7:9"), (GHC78, "A.hs@7:1-7:15")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (7,12,7,15) "Int" TcClsName "" "ghc-prim-0.2.0.0:GHC.Types" "<wired into compiler>" "base-4.5.1.0:Data.Int" "wired in to the compiler"
-        _fixme session "#8607" $ assertIdInfo session "A" (8,13,8,16) "Bar" TcClsName "" "main:A" "A.hs@8:13-8:16" "" "binding occurrence"
+        assertIdInfo' session "A" (8,13,8,16) (8,13,8,16) "Bar" TcClsName [] "main:A" [(GHC742, "A.hs@8:13-8:16"), (GHC78, "A.hs@8:1-8:18")] "" (allVersions "binding occurrence")
         assertIdInfo session "A" (8,17,8,18) "a" TvName "" "main:A" "A.hs@8:17-8:18" "" "binding occurrence"
         assertIdInfo' session "A" (9,15,9,18) (9,15,9,18) "Bar" TcClsName [] "main:A" [(GHC742, "A.hs@8:13-8:16"), (GHC78, "A.hs@8:1-8:18")] "" (allVersions "defined locally")
         assertIdInfo session "A" (9,19,9,22) "Int" TcClsName "" "ghc-prim-0.2.0.0:GHC.Types" "<wired into compiler>" "base-4.5.1.0:Data.Int" "wired in to the compiler"
@@ -4318,9 +4314,9 @@ syntheticTests = [
               ]
 
         do useSites <- getUseSites session
-           _fixme session "use" $ assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F
+           assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F
            assertUseSites useSites "A" (2, 14, 2, 17) "Int" uses_Int
-           _fixme session "use" $ assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G
+           assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G
            assertUseSites useSites "B" (3,  6, 3,  7) "H"   uses_H
            assertUseSites useSites "B" (3, 14, 3, 17) "Int" uses_Int
            assertUseSites useSites "B" (3, 18, 3, 19) "F"   uses_F
@@ -4348,9 +4344,9 @@ syntheticTests = [
               ]
 
         do useSites <- getUseSites session
-           _fixme session "use" $ assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F2
+           assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F2
            assertUseSites useSites "A" (2, 14, 2, 17) "Int" uses_Int
-           _fixme session "use" $ assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G2
+           assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G2
            assertUseSites useSites "B" (3,  6, 3,  7) "H"   uses_H
            assertUseSites useSites "B" (3, 14, 3, 17) "Int" uses_Int
            assertUseSites useSites "B" (3, 18, 3, 19) "G"   uses_G2
@@ -4379,9 +4375,9 @@ syntheticTests = [
               ]
 
         do useSites <- getUseSites session
-           _fixme session "use" $ assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F3
+           assertUseSites useSites "A" (2,  6, 2,  7) "F"   uses_F3
            assertUseSites useSites "A" (2, 14, 2, 17) "Int" uses_Int3
-           _fixme session "use" $ assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G2
+           assertUseSites useSites "A" (3,  6, 3,  7) "G"   uses_G2
            assertUseSites useSites "B" (3,  6, 3,  7) "H"   uses_H
            assertUseSites useSites "B" (3, 14, 3, 17) "Int" uses_Int3
            assertUseSites useSites "B" (3, 18, 3, 19) "G"   uses_G2
@@ -5724,13 +5720,8 @@ instance IgnoreVersions PackageId where
 -- ([] if the bug is unrelated to the GHC version)
 knownProblems :: [(String, [GhcVersion])]
 knownProblems = [
-    -- https://ghc.haskell.org/trac/ghc/ticket/8607
-    ("#8607", [GHC78])
-    -- For some reason we don't report all use sites correctly, I'm not
-    -- yet completely sure why (this *might* be related to #8607)
-  , ("use", [GHC78])
     -- https://github.com/fpco/ide-backend/issues/146
-  , ("#146", [])
+    ("#146", [])
     -- https://github.com/fpco/ide-backend/issues/32
   , ("#32", [])
     -- https://github.com/fpco/ide-backend/issues/150
