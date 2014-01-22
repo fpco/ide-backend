c2024964adf51793aea0fdc04201b589b2d1a0fc
Mikolaj Konarski <mikolaj@well-typed.com>
2014-01-22 14:19:54 +0100

    repro issue167

--- test/typecheck-dir.hs
+++ test/typecheck-dir.hs
@@ -1,92 +1,113 @@
-module Main where
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell, TypeSynonymInstances, FlexibleInstances, RecordWildCards, OverlappingInstances #-}
 
-import Control.Exception (bracket)
-import Control.Monad (liftM, unless)
-import Data.Monoid (mconcat)
+
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Time
+import Data.Monoid
+import qualified Data.ByteString.Lazy.Char8 as L
+import System.IO (appendFile)
+import Control.Monad (forM_)
+import IdeSession hiding (defaultSessionConfig, defaultSessionInitParams)
+import qualified IdeSession as IdeSession
+
+import Control.Concurrent (threadDelay)
+import qualified Control.Exception as Ex
+import Control.Monad
+import Control.DeepSeq (rnf)
+import qualified Data.ByteString.Char8      as BSSC
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Lazy.Char8 as BSLC
+import qualified Data.ByteString.Lazy.UTF8  as BSL8
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Data.List (isInfixOf, isPrefixOf, isSuffixOf, sort, elemIndex)
+import qualified Data.List as List
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Data.Monoid (mconcat, mempty, (<>))
+import Data.Text (Text)
+import Data.Char (isLower, isSpace)
+import Data.Either (lefts)
+import qualified Data.Text as Text
+import Debug.Trace (traceEventIO)
+import Data.Version (Version (..))
+import Distribution.License (License (..))
+import Distribution.Simple.Program.Find ( -- From our patched cabal
+    ProgramSearchPath
+  , findProgramOnSearchPath
+  , ProgramSearchPathEntry(..)
+  )
+import Prelude hiding (mod, span)
 import System.Directory
-import System.Environment
+import qualified System.Environment as System.Environment
+import System.Exit (ExitCode (..))
+import System.FilePath
 import System.FilePath.Find (always, extension, find)
-import System.IO.Temp (withTempDirectory)
+import System.IO as IO
+import System.IO.Temp (withTempDirectory, createTempDirectory)
+import System.IO.Unsafe (unsafePerformIO)
+-- import System.Process (readProcess)
+-- import qualified System.Process as Process
+-- import System.Random (randomRIO)
+import System.Timeout (timeout)
+-- import Text.Regex (mkRegex, subRegex)
 
-import IdeSession
 
---- A sample program using the library. It type-checks all files
---- in the given directory and prints out the list of errors.
 
--- | Some common extensions, etc. (please fill in).
--- Curiously "-XTypeFamilies" causes the type-checking of the default
--- test file to fail. The same file type-checks OK with the ghc-errors
--- test program (with no GHC extensions set).
-defOpts :: [String]
-defOpts = [ "-hide-all-packages"  -- make sure we don't depend on local pkgs
-          , "-XCPP"
-          , "-XNoTemplateHaskell"  -- TH not available when profiling
-          , "-XBangPatterns"
-          , "-XRecordWildCards"
-          , "-XNamedFieldPuns"
-          , "-XPatternGuards"
-          , "-XScopedTypeVariables"
-          , "-XMultiParamTypeClasses"
-          , "-XRankNTypes"
--- causes problems with the Cabal code:          , "-XTypeFamilies"
-          , "-XForeignFunctionInterface"
-          , "-XDeriveDataTypeable"
-          , "-package template-haskell"
-          , "-package old-time"
-          , "-package parallel"
-          , "-package base"
-          , "-package deepseq"
-          , "-package filepath"
-          , "-package directory"
-          , "-package process"
-          , "-package time"
-          , "-package containers"
-          , "-package array"
-          , "-package pretty"
-          , "-package bytestring"
-          , "-package unix"
-          ]
+mkFile :: Int -> (FilePath, L.ByteString)
+mkFile i =
+    (name, contents)
+  where
+    name = "Module" ++ show i ++ ".hs"
+    contents
+        | i == 1 = L.pack $ unlines
+            [ "module Module1 where"
+            , "val1 = 1"
+            ]
+        | otherwise = L.pack $ unlines
+            [ "{-# LANGUAGE TemplateHaskell #-}"
+            , "module Module" ++ show i ++ " where"
+            , "import Module" ++ show (i - 1)
+            , "import System.IO (appendFile)"
+            , "import Language.Haskell.TH.Syntax"
+            , "qRunIO (appendFile \"../log.txt\" \"TH: Compiling " ++ show i ++ "\\n\") >> return []"
+            , "val" ++ show i ++ " = val" ++ show (i - 1)
+            ]
+
+say :: String -> IO ()
+say msg = do
+    now <- getCurrentTime
+    appendFile "log.txt" $ show now ++ ": " ++ msg ++ "\n"
+    print $ show now ++ ": " ++ msg ++ "\n"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let (originalSourcesDir, opts) = case args of
-        ["--help"] ->
-          error "usage: typecheck-dir [source-dir [ghc-options]]"
-        [dir] -> (dir, defOpts)
-        dir : optsArg -> (dir, optsArg)
-        [] -> ("test/Cabal",
-               defOpts)
-  slashTmp <- getTemporaryDirectory
-  withTempDirectory slashTmp "typecheck-dir."
-    $ check opts originalSourcesDir
+    say "Entering main"
+    session <- initSession (SessionInitParams Nothing) defaultSessionConfig
+    say "Session initialized"
 
-check :: [String] -> FilePath -> FilePath -> IO ()
-check opts what configDir = do
-  putStrLn $ "Copying files from: " ++ what ++ "\n"
-          ++ "to a temporary directory at: " ++ configDir ++ "\n"
-  -- Init session.
-  let sessionConfig = defaultSessionConfig{
-                          configDir
-                     -- , configInProcess  = True
-                        , configStaticOpts = opts
-                        }
+    forM_ [1..10000] $ \i -> do
+        let (name, contents) = mkFile i
+        let update = updateSourceFile name contents
+        say $ "applying " ++ show i
+        updateSession session update $ \p -> say $ "progress == " ++ show p
+        say $ "applied " ++ show i
 
-  bracket (initSession defaultSessionInitParams sessionConfig)
-          shutdownSession $ \session -> do
-    isFile      <- doesFileExist      what
-    isDirectory <- doesDirectoryExist what
+defaultSessionInitParams :: SessionInitParams
+{-# NOINLINE defaultSessionInitParams #-}
+defaultSessionInitParams = unsafePerformIO $
+  return IdeSession.defaultSessionInitParams
 
-    unless (isFile || isDirectory) $ fail ("invalid argument " ++ what)
-
-    modules <- if isFile
-      then return [what]
-      else find always ((`elem` sourceExtensions) `liftM` extension) what
-
-    print modules
-
-    let update = mconcat (map updateSourceFileFromFile modules)
-
-    updateSession session update print
-    errs <- getSourceErrors session
-    putStrLn $ "\nErrors and warnings:\n" ++ unlines (map show errs)
+defaultSessionConfig :: SessionConfig
+{-# NOINLINE defaultSessionConfig #-}
+defaultSessionConfig = unsafePerformIO $ do
+  packageDb     <- (System.Environment.getEnv "IDE_BACKEND_PACKAGE_DB")
+                     `Ex.catch` (\(_ :: Ex.IOException) -> return "")
+  extraPathDirs <- (System.Environment.getEnv "IDE_BACKEND_EXTRA_PATH_DIRS")
+                     `Ex.catch` (\(_ :: Ex.IOException) -> return "")
+  let packageDbStack
+        | null packageDb = configPackageDBStack IdeSession.defaultSessionConfig
+        | otherwise      = [GlobalPackageDB, SpecificPackageDB packageDb]
+  return IdeSession.defaultSessionConfig {
+             configPackageDBStack = packageDbStack
+           , configExtraPathDirs  = splitSearchPath extraPathDirs
+           }
