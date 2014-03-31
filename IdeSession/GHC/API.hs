18660543644c16b0f132fc170999a7547309141b
Mikolaj Konarski <mikolaj@well-typed.com>
2014-03-31 22:22:17 +0200

    Bump API version number after the targets sent to the server changed

--- IdeSession/GHC/API.hs
+++ IdeSession/GHC/API.hs
@@ -27,7 +27,7 @@ import IdeSession.GHC.Responses
 -- We use a Unix timestamp for this so that these API versions have some
 -- semantics (http://www.epochconverter.com/, GMT).
 ideBackendApiVersion :: Int
-ideBackendApiVersion = 1391084140
+ideBackendApiVersion = 1396297257
 
 {------------------------------------------------------------------------------
   Configuration
