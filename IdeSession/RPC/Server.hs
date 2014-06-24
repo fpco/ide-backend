1d4d1c56eca9f1c4546c3020ef719943e574e011
Edsko de Vries <edsko@well-typed.com>
2014-06-24 14:36:08 +0100

    Wait for pending msgs to be sent on server exit
    This should fix the non-deterministic bug that Mikolaj and I were seeing (for
    instance with #58).

--- IdeSession/RPC/Server.hs
+++ IdeSession/RPC/Server.hs
@@ -16,16 +16,16 @@ import System.IO
   )
 import System.Posix.Types (Fd)
 import System.Posix.IO (closeFd, fdToHandle)
-import Control.Monad (forever)
+import Control.Monad (void)
 import qualified Control.Exception as Ex
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, writeChan)
 import qualified Data.ByteString.Lazy.Char8 as BSL
-import Control.Concurrent.Async (async)
+import Control.Concurrent.Async (Async, async)
 import Data.Binary (encode, decode)
 import GHC.IO.Handle.FD (openFileBlocking)
 
-import IdeSession.Util.BlockingOps (readChan, waitAnyCatchCancel)
+import IdeSession.Util.BlockingOps (readChan, wait, waitAny)
 import IdeSession.RPC.API
 import IdeSession.RPC.Stream
 
@@ -74,60 +74,153 @@ rpcServer' :: Handle                     -- ^ Input
            -> IO ()
 rpcServer' hin hout herr server = do
     requests   <- newChan :: IO (Chan BSL.ByteString)
-    responses  <- newChan :: IO (Chan BSL.ByteString)
+    responses  <- newChan :: IO (Chan (Maybe BSL.ByteString))
 
     setBinaryBlockBuffered [hin, hout, herr]
 
-    reader  <- async $ readRequests hin requests
-    writer  <- async $ writeResponses responses hout
-    handler <- async $ channelHandler requests responses server
+    -- Each thread installs it own exception handler before unmasking
+    -- asynchronous exceptions. This way when an exception occurs we can
+    -- identify it (by looking at which ServerEvent was returned).
+    (reader, writer, handler) <- Ex.mask $ \restore -> do
+      reader  <- async $ readRequests   restore hin requests
+      writer  <- async $ writeResponses restore responses hout
+      handler <- async $ channelHandler restore requests responses server
+      return (reader, writer, handler)
 
-    $waitAnyCatchCancel [reader, writer, handler] >>= tryShowException . snd
+    (_thread, ev) <- $waitAny [reader, writer, handler]
+    case ev of
+      -- If we lose connection with the client, just terminate.
+      -- See #194 (in particular, https://github.com/fpco/ide-backend/issues/194#issuecomment-44210412)
+      LostConnection ex ->
+        tryShowException (Just ex)
+
+      -- If the client requests termination, we simply terminate immediately.
+      -- It is the client's responsibility to have a proper shutdown protocol
+      -- with the server thread
+      ReaderThreadTerminated ->
+        return ()
+
+      -- The writer thread should never terminate normally unless we request
+      -- it; this is a logical impossibility :)
+      WriterThreadTerminated ->
+        error "The impossible happened"
+
+      -- When the main server thread terminates we ask the writer thread to
+      -- terminate so that we make sure to send any pending messages
+      ServerThreadTerminated ->
+        tryShowException =<< flushResponses responses writer
+
+      -- When the main server thread aborts, we still attempt to flush any
+      -- remaining messages, but the exception that we record is the one from
+      -- the server (the writer thread might terminate with a further exception)
+      ServerThreadAborted ex -> do
+        tryShowException (Just ex)
+        void $ flushResponses responses writer
+
     threadDelay 100000
   where
-    tryShowException :: Either Ex.SomeException () -> IO ()
-    tryShowException (Left ex) =
+    tryShowException :: Maybe Ex.SomeException -> IO ()
+    tryShowException (Just ex) =
       -- We don't want to throw an exception showing the previous exception
       ignoreIOExceptions $ hPutFlush herr . BSL.pack . show $ ex
-    tryShowException (Right ()) =
+    tryShowException Nothing =
       return ()
 
 --------------------------------------------------------------------------------
 -- Internal                                                                   --
 --------------------------------------------------------------------------------
 
+-- | We record the reason why the various threads are terminating, so that we
+-- can take the appropriate action
+data ServerEvent =
+    -- | The reader thread terminates when the client sends a 'RequestShutdown'
+    -- message
+    ReaderThreadTerminated
+
+    -- | After the main server thread terminates, we wait for the writer thread
+    -- to terminate to make sure there are no pending unsent messages
+  | WriterThreadTerminated
+
+    -- | Termination of the main server thread
+  | ServerThreadTerminated
+
+    -- | Main server thread threw an exception
+  | ServerThreadAborted Ex.SomeException
+
+    -- | The reader thread and writer threads terminate with 'LostConnection'
+    -- if an exception occurs
+  | LostConnection Ex.SomeException
+
 -- | Decode messages from a handle and forward them to a channel.
 -- The boolean result indicates whether the shutdown is forced.
-readRequests :: Handle -> Chan BSL.ByteString -> IO ()
-readRequests h ch = newStream h >>= go
+readRequests :: Restore -> Handle -> Chan BSL.ByteString -> IO ServerEvent
+readRequests restore h ch =
+    Ex.handle (return . LostConnection)
+              (restore (newStream h >>= go))
   where
-    go :: Stream Request -> IO ()
+    go :: Stream Request -> IO ServerEvent
     go input = do
       req <- nextInStream input
       case req of
         Request req'         -> writeChan ch (unIncBS req') >> go input
-        RequestShutdown      -> return ()
+        RequestShutdown      -> return ReaderThreadTerminated
 
 -- | Encode messages from a channel and forward them on a handle
-writeResponses :: Chan BSL.ByteString -> Handle -> IO ()
-writeResponses ch h = forever $ do
-  bs <- $readChan ch
-  hPutFlush h $ encode (Response (IncBS bs))
+--
+-- Terminates on 'Nothing'.
+writeResponses :: Restore -> Chan (Maybe BSL.ByteString) -> Handle -> IO ServerEvent
+writeResponses restore ch h =
+    Ex.handle (return . LostConnection)
+              (restore go)
+  where
+    go :: IO ServerEvent
+    go = do
+      mbs <- $readChan ch
+      case mbs of
+        Just bs -> do hPutFlush h $ encode (Response (IncBS bs)) ; go
+        Nothing -> return WriterThreadTerminated
+
+-- | Ask the writer thread to terminate and wait for all remaining messages to
+-- have been sent. Returns 'Nothing' if the writer thread terminated normally,
+-- or the exception if it didn't.
+flushResponses :: Chan (Maybe BSL.ByteString) -> Async ServerEvent -> IO (Maybe Ex.SomeException)
+flushResponses responses writer = do
+  writeChan responses Nothing
+  ev <- $wait writer
+  case ev of
+    WriterThreadTerminated ->
+      return Nothing
+    LostConnection ex ->
+      return (Just ex)
+    _ ->
+      error "the impossible happened"
 
 -- | Run a handler repeatedly, given input and output channels
-channelHandler :: Chan BSL.ByteString
+channelHandler :: Restore
                -> Chan BSL.ByteString
+               -> Chan (Maybe BSL.ByteString)
                -> (RpcConversation -> IO ())
-               -> IO ()
-channelHandler requests responses server =
-  server RpcConversation {
-      get = $readChan requests >>= Ex.evaluate . decode
-    , put = writeChan responses . encode
-    }
+               -> IO ServerEvent
+channelHandler restore requests responses server =
+    Ex.handle (return . ServerThreadAborted)
+              (restore go)
+  where
+    go :: IO ServerEvent
+    go = do
+      server RpcConversation {
+          get = $readChan requests >>= Ex.evaluate . decode
+        , put = writeChan responses . Just . encode
+        }
+      return ServerThreadTerminated
+
+--------------------------------------------------------------------------------
+-- Auxiliary                                                                  --
+--------------------------------------------------------------------------------
+
+type Restore = forall a. IO a -> IO a
 
 -- | Set all the specified handles to binary mode and block buffering
 setBinaryBlockBuffered :: [Handle] -> IO ()
 setBinaryBlockBuffered =
   mapM_ $ \h -> do hSetBinaryMode h True
                    hSetBuffering  h (BlockBuffering Nothing)
-
