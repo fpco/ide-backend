f8c872d3969091f969ae8fe0f31f995588adc840
Mikolaj Konarski <mikolaj@well-typed.com>
2014-07-04 11:24:55 +0200

    Reset handles even when snippet interrupted
    Otherwise the test
    "First snippet closes stdin (interrupted 'interact'); next snippet unaffected"
    fails.

--- rts/IdeBackendRTS.hs
+++ rts/IdeBackendRTS.hs
@@ -27,12 +27,12 @@ import qualified GHC.IO.FD as FD
 
 run :: RunBufferMode -> RunBufferMode -> IO a -> IO a
 run outBMode errBMode io = do
-  res <- 
-    withBuffering IO.stdout outBMode $ withBuffering IO.stderr errBMode $ io
-  resetStdin  IO.utf8
-  resetStdout IO.utf8
-  resetStderr IO.utf8
-  return res
+  let resetHandles = do
+        resetStdin  IO.utf8
+        resetStdout IO.utf8
+        resetStderr IO.utf8
+  withBuffering IO.stdout outBMode (withBuffering IO.stderr errBMode io)
+    `Ex.finally` resetHandles
 
 {-------------------------------------------------------------------------------
   Buffer modes
