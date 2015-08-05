9b1bb29e556d1c106af3c836962263064e9d4508
Michael Sloan <mgsloan@gmail.com>
2015-08-04 18:08:27 -0700

    Bump ide-backend API version

--- ide-backend-common/IdeSession/GHC/API.hs
+++ ide-backend-common/IdeSession/GHC/API.hs
@@ -31,7 +31,7 @@ import IdeSession.GHC.Responses
 -- We use a Unix timestamp for this so that these API versions have some
 -- semantics (http://www.epochconverter.com/, GMT).
 ideBackendApiVersion :: Int
-ideBackendApiVersion = 1426765899
+ideBackendApiVersion = 1438736884
 
 {------------------------------------------------------------------------------
   Configuration
