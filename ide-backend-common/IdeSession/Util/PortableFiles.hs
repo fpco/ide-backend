583ed0d27cf3decdc45b53ff27cd0b9da2d1f560
Daniel Beskin <daniel.beskin@gmail.com>
2015-08-09 09:58:18 +0300

    Fixing the `setFileTimes` function so that it works for directories on Windows

--- ide-backend-common/IdeSession/Util/PortableFiles.hs
+++ ide-backend-common/IdeSession/Util/PortableFiles.hs
@@ -13,8 +13,10 @@ import System.PosixCompat.Files hiding (setFileTimes)
 import qualified System.Posix.Files as Posix
 #else
 import Foreign
-import Foreign.C
-import System.Posix.Internals
+import Data.Time.Clock.POSIX
+import System.FilePath.Posix
+import qualified System.Win32 as Win32
+import Control.Exception (bracket)
 #endif
 
 setFileTimes :: FilePath -> EpochTime -> EpochTime -> IO ()
@@ -25,15 +27,29 @@ setFileTimes = Posix.setFileTimes
 
 #else
 
--- Mostly copied from System.Posix.Files
-setFileTimes name atime mtime =
-  withCString name $ \s ->
-   allocaBytes 16 $ \p -> do
-     (\hsc_ptr -> pokeByteOff hsc_ptr 0)  p atime
-     (\hsc_ptr -> pokeByteOff hsc_ptr 8) p mtime
-     throwErrnoPathIfMinus1_ "setFileTimes" name (c__utime s p)
-
-foreign import ccall unsafe "utime.h _utime"
-   c__utime :: CString -> Ptr CUtimbuf -> IO CInt
+-- Unlike `utime`, which doesn't work for directories on Windows, this works for both for files and directories
+-- Mostly copied from @System.Directory@ 1.2.3.0
+setFileTimes path atime mtime =
+  bracket (openFileHandle path' Win32.gENERIC_WRITE)
+          Win32.closeHandle $ \ handle ->
+    with (posixToWindowsTime atime) $ \ atime' ->
+      with (posixToWindowsTime mtime) $ \ mtime' ->
+        Win32.failIf_ not "setFileTimes" $
+          Win32.c_SetFileTime handle nullPtr atime' mtime'
+  where
+    path'  = normalise path             -- handle empty paths
+    windowsPosixEpochDifference :: Num a => a
+    windowsPosixEpochDifference = 116444736000000000
+    posixToWindowsTime :: EpochTime -> Win32.FILETIME
+    posixToWindowsTime t = Win32.FILETIME $
+      truncate ((realToFrac t :: POSIXTime) * 10000000 + windowsPosixEpochDifference)
+    openFileHandle :: String -> Win32.AccessMode -> IO Win32.HANDLE
+    openFileHandle p mode = Win32.createFile p mode share Nothing
+                                             Win32.oPEN_EXISTING flags Nothing
+      where share =  Win32.fILE_SHARE_DELETE
+                 .|. Win32.fILE_SHARE_READ
+                 .|. Win32.fILE_SHARE_WRITE
+            flags =  Win32.fILE_ATTRIBUTE_NORMAL
+                 .|. Win32.fILE_FLAG_BACKUP_SEMANTICS -- required for directories
 
 #endif
