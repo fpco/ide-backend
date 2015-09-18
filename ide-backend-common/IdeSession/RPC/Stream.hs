7f12bfb87515f2f21cec1a4c7aeaa67cc9dcbde3
Daniel Beskin <daniel.beskin@gmail.com>
2015-09-18 08:00:45 +0300

    Checking whether a handle in Stream.hs is closed before using it
    This helps solving some race conditions in the tests in SessionRestart.hs

--- ide-backend-common/IdeSession/RPC/Stream.hs
+++ ide-backend-common/IdeSession/RPC/Stream.hs
@@ -7,7 +7,7 @@ module IdeSession.RPC.Stream (
   ) where
 
 import Prelude hiding (take)
-import System.IO (Handle)
+import System.IO (Handle, hIsEOF)
 import qualified Control.Exception as Ex
 import qualified Data.ByteString.Lazy.Internal as BSL
 import qualified Data.ByteString as BSS
@@ -31,22 +31,30 @@ nextInStream (Stream h st) = readIORef st >>= go False
     go atEnd decoder = case decoder of
       Binary.Fail _ _ err -> do
         writeIORef st decoder
-        Ex.throwIO $ userError $
+        throwUserError $
           if atEnd
             then "IdeSession.RPC.Stream ended, causing: " ++ err
             else "IdeSession.RPC.Stream decode failure: " ++ err
       Binary.Partial k -> do
-        mchunk <- Ex.try $ BSS.hGetSome h BSL.defaultChunkSize
-        case mchunk of
-          Left (ex :: Ex.SomeException) -> do
-            writeIORef st decoder
-            -- wrapping with a user error so that it's easier to catch later on
-            Ex.throwIO $ userError $ "IdeSession.RPC.Stream ended unexpectedly, causing: " ++ show ex
-          Right chunk | BSS.null chunk -> go True . k $ Nothing
-                      | otherwise      -> go False . k $ Just chunk
+        eof <- hIsEOF h
+        -- For some reason doing this check helps avoiding race conditions in
+        -- in various scenarios when the server is force terminated.
+        -- E.g., the tests in SessionRestart.hs
+        if eof then throwUserError "IdeSession.RPC.Stream input handle closed unexpectedly"
+        else do
+          mchunk <- Ex.try $ BSS.hGetSome h BSL.defaultChunkSize
+          case mchunk of
+            Left (ex :: Ex.SomeException) -> do
+              writeIORef st decoder
+              -- wrapping with a user error so that it's easier to catch later on
+              throwUserError $ "IdeSession.RPC.Stream ended unexpectedly, causing: " ++ show ex
+            Right chunk | BSS.null chunk -> go True . k $ Nothing
+                        | otherwise      -> go False . k $ Just chunk
       Binary.Done unused _numConsumed a -> do
         writeIORef st $ contDecoder unused
         return a
 
     contDecoder :: BSS.ByteString -> Binary.Decoder a
     contDecoder = Binary.pushChunk (Binary.runGetIncremental Binary.get)
+
+    throwUserError = Ex.throwIO . userError
